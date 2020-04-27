08277c20e6d6fd2093d1141e158b40b3630bc5c4
ddmjohn <johnskag12@gmail.com>
2020-04-27 21:19:28 +1200

    added header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/original.svg';
 
 const Header = () => (
     <div className='header'>
         <Link to="/">
-            <span className="logo">{"MY STORE"}</span>
+           <Logo className='logo'/>
         </Link>
         <div className="options">
             <Link className='option' to='/shop'>SHOP</Link>
