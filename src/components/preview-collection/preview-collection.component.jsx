5ac932fe449e9dcbe168c7236eea803077e94f12
ddmjohn <johnskag12@gmail.com>
2020-04-27 20:36:07 +1200

    added shop page

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import CollectionItem from "../collection-item/collection-item.component.jsx";
 import './preview-collection.styles.scss';
 
 const PreviewCollection = ({title, items}) => (
 
 <div className='preview-collection'>
     <h1 className='title'>{title.toUpperCase()}</h1>
-    <div class='preview'>
-    {items.map((item)=>(
-        <div key={item.id}>{item.name}</div>
+    <div className='preview'>
+    {items
+    .map(({id,...otherItemProps})=>(
+        <CollectionItem key={id} {...otherItemProps}/>
     ))}
     </div>    
   </div>
